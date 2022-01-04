4abf3d4700c340fd2eab8576ccc67ceb3d8c6a79
Masood Fallahpoor <masood.fallahpoor@gmail.com>
2022-01-04 15:00:39 +0100

    Update LooperWorkerThread

--- app/src/main/java/ir/fallahpoor/workerthread/LooperWorkerThread.kt
+++ app/src/main/java/ir/fallahpoor/workerthread/LooperWorkerThread.kt
@@ -3,7 +3,7 @@ package ir.fallahpoor.workerthread
 import android.os.Handler
 import android.os.Looper
 
-class LooperWorkerThread(threadName: String) : Thread(threadName) {
+class LooperWorkerThread : Thread() {
 
     private lateinit var handler: Handler
 
@@ -13,24 +13,23 @@ class LooperWorkerThread(threadName: String) : Thread(threadName) {
 
     override fun run() {
 
-        // Preparing a looper on current thread.
+        // Prepare a looper on the current thread.
         // The current thread is being detected implicitly
         Looper.prepare()
 
         // Attach the Handler to the Looper of current thread
         handler = Handler(Looper.myLooper())
 
-        // After the following line the thread will start
+        // After the following line this thread will start
         // running the message loop and will not normally
         // exit the loop unless a problem happens or you
-        // quit() the looper
+        // call quit()
         Looper.loop()
 
     }
 
-    fun execute(task: Runnable): LooperWorkerThread {
+    fun execute(task: Runnable) {
         handler.post(task)
-        return this
     }
 
     fun quit() {
